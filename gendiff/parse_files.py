a5df38240d55e6b170a99441cfaa54a79a1d124c
Aid <kostyuchik22@gmail.com>
2023-11-08 20:09:49 +0300

    add files

--- gendiff/parse_files.py
+++ gendiff/parse_files.py
@@ -1,6 +1,10 @@
 import json
+import yaml
 
 
 # Open file and return dict
 def open_file(file_path):
-    return json.load(open(file_path))
+    print(file_path)
+    if file_path[-5:] == '.json':
+        return json.load(open(file_path))
+    return yaml.load(open(file_path), Loader=yaml.FullLoader)
--- tests/test_float_file_comparsion.py
+++ tests/test_float_file_comparsion.py
@@ -2,7 +2,7 @@ from gendiff.parse_files import open_file
 from gendiff import generate_diff
 
 
-def test_parse_files():
+def test_parse_json_files():
     first_file = 'tests/fixtures/file1.json'
     second_file = 'tests/fixtures/file2.json'
     diff_list = generate_diff(first_file, second_file)
@@ -16,3 +16,14 @@ def test_parse_files():
                      '  + verbose: True\n'\
                      '}'
     assert correct_output == diff_list
+
+
+def test_parse_yml_files():
+    first_file = 'tests/fixtures/file1.yml'
+    second_file = 'tests/fixtures/file2.yaml'
+    correct_file = open('tests/fixtures/correct_yml.txt').readlines()
+    correct_output = ''.join(correct_file)
+    assert generate_diff(first_file, second_file) == correct_output.lower()
+
+
+test_parse_yml_files()
