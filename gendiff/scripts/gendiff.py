c309cb8c68d17f77b25164341076587bdfe2789b
Aid <kostyuchik22@gmail.com>
2023-12-25 14:42:58 +0300

    mod gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,10 +8,10 @@ def main():
         description='Compares two configuration files and shows a difference.')
     diff.add_argument('first_file', type=str, help='first configuration file')
     diff.add_argument('second_file', type=str, help='second configuration file')
-    diff.add_argument('-f', '--format',
-                      help='set format of output (default: stylish)')
+    diff.add_argument('-f', '--format', choices=['stylish', 'plain', 'json'],
+                      help='set format of output (default: stylish)', default='stylish')
     args = diff.parse_args()
-    return generate_diff(args.first_file, args.second_file, stylish)
+    return generate_diff(args.first_file, args.second_file, args.format)
 
 
 if __name__ == '__main__':
