a82748f206199ac097642ffe21e49eeb47674ad8
Aid <kostyuchik22@gmail.com>
2023-12-25 15:32:16 +0300

    mod file

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -55,6 +55,5 @@ def test_generate_diff(test_input1, test_input2, output, expected):
         result_data = file.read()
     test_path1 = get_path(test_input1)
     test_path2 = get_path(test_input2)
-    print(generate_diff(test_path1, test_path2, output))
     assert result_data == generate_diff(test_path1, test_path2, output)
 
